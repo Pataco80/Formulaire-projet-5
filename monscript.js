d99faa229035f2fe7527fdc6fab17624749a54ad
Pataco80 <pataco80@gmail.com>
2017-01-05 21:45:28 +0100

    amélioration mais quelques modifications à apporter

--- monscript.js
+++ monscript.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
 	// Liste des variables de champs
-	var firstname = $('#first-name'),
+	/*var firstname = $('#first-name'),
 			lastname = $('#last-name'),
 			email = $('#email'),
 			service = $('#service'),
 			sujet = $('#sujet'),
-			message = $('#message');
+			message = $('#message');*/
 	
 	// Liste de variables d'attributs
-	var champ_obligatoire = $("[required]");
-	var champ_alpha = $("input[type:text]").data("validation","alpha");
+	//var champ_obligatoire = $("*").data("data-validation","true");
+	//var champ_alpha = $("input[type:text]").data("validation","alpha");
 	
-console.log(champ_alpha);
+
+	$(function(){
+		var requis = $(".form-control").data("validation") === true;
+		$("requis").blur(function(){
+			if ($(this).val() == "") {
+					$(this).css("background-color", "red");
+			}else {
+				$(this).css("background-color", "yellow");
+			}
+		});
+		console.log("#first-name");
+	});
+	
 });
