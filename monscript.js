aef1e6430a52208658827a3accbcb4b78a6f7ccd
Pataco80 <pataco80@gmail.com>
2017-01-11 11:56:37 +0100

    Modification du code pour une diminution de traitement

--- monscript.js
+++ monscript.js
@@ -1,26 +1,121 @@
-$(document).ready(function(){
-	// Liste des variables de champs
-	var $champs	= $('input', 'textarea', 'select');
-			
-	
-	
-	function haserror($champs){
-			$(this).parent(".form-group").addClass("has-error");
-			$(this).siblings(".control-label").addClass("has-error");
-			$(this).next(".glyphicon").addClass("glyphicon-remove");
-			$(this).siblings(".help-block").text("Veuillez");
-	}
-	
-	
-	$(function(){
-		$champs.blur(function(e){
-			e.preventDefault();
-			if ($champs.is("required").val() == ""){
-				haserror($champs);
-			}else{
-				$(this).css({borderColor:'green'});
-			}
-		});
-	});
-	
+@@ -1,78 +1,84 @@
+$(document).ready(function() {
+    // Liste des variables
+    var champ_requis = $('input', 'select', 'textarea').attr('required');
+    var champ_alpha = $('input').attr('patern', 'alpha');
+    var champ_email = $('input[type=email]');
+    
+    var champ_requis = $(':input[required]');
+    console.log(champ_requis);
+
+    
+    // Liste des pattern
+    /* Liste des pattern
+    var pattern_alpha = "/^[a-z\’\-\ ]$/i";
+    var pattern_email = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i";
+    */
+    
+    
+    /* Si un champ de texte obligatoire est vide, la fonction suivante sera executée.
+    - la div parente contenant " class="form-group has-feedback" " auront la class "has-error" qui viendra s'ajouter donnant une bordure rouge au champ selectioné.
+    - La balise <span> suivante contentant " class="glyphicon" " aura la class "glyphicon-remove" afin d'afficher la croix rouge
+    - La div " class="help-block" " affichera le texte "Ce champ est obligatoire !"
+    */
+    
+    function input_vide(){
+        $(this).parent().addClass('has-error'), $(this).siblings('.glyphicon').addClass('glyphicon-remove'), $(this).siblings('.help-block').text("Ce champ est obligatoire !");
+        $(this).parent().addClass('has-error');
+        $(this).siblings('.glyphicon').addClass('glyphicon-remove');
+        $(this).siblings('.help-block').text("Ce champ est obligatoire !");
+    }
+    input_vide();
+    
+    // Si ue addresse e-mail est mal renseignée, l'erreur sera identique à un champ vide mais avec un texte de la div " class="help-block" " affichera "Addresse mail invalide !"
+    function mail_invalid(){
+        $(this).parent().addClass('has-error'), $(this).siblings('.glyphicon').addClass('glyphicon-remove'), $(this).siblings('.help-block').text("Addresse mail invalide !");
+    }
+    mail_invalid();
+
+    
+ 
+    /* Si un champ de texte n'a pas les charactères alphabétiques autorisés, la fonction suivante sera executée.
+    - la div parente contenant " class="form-group has-feedback" " auront la class "has-warning" qui viendra s'ajouter donnant une bordure orange au champ selectioné.
+    - La balise <span> suivante contentant " class="glyphicon" " aura la class "glyphicon-warning-sign" afin d'afficher un triangle orange
+    - La div " class="help-block" " affichera le texte "Seuls les charactères alphabétiques sont acceptés."
+    */
+    
+    
+    function erreur_alpha(){
+        $(this).parent().addClass('has-warning'), $(this).siblings('.glyphicon').addClass('glyphicon-warning-sign'), $(this).siblings('.help-block').text("Seuls les charactères alphabétiques sont acceptés.");
+    }
+    erreur_alpha();
+
+    
+    
+    /* Si un champ de texte est correctement remplis, la fonction suivante sera executée.
+    - la div parente contenant " class="form-group has-feedback" " aura la class "has-success" qui viendra s'ajouter donnant une bordure verte au champ selectioné.
+    - La balise <span> suivante contentant " class="glyphicon" " aura la class "glyphicon-ok" afin d'afficher un vu de couleur vert.
+    - La div " class="help-block" n'affichera aucun texte.
+    */
+    
+    
+    function input_success(){
+        $(this).parent().addClass('has-success'), $(this).siblings('.glyphicon').addClass('glyphicon-ok'), $(this).siblings('.help-block').css('display:none');
+    }
+    input_success();
+
+    
+    
+    // Si dessous le champ selectioné aura l'apparence de base.
+    function input_default(){
+        $(this).parent().removeClass('has-error', 'has-warning', 'has-success'), $(this).siblings('.glyphicon').removeClass('glyphicon-remove', 'glyphicon-warning-sign', 'glyphicon-ok'), $(this).siblings('.help-block').css('display:none');
+    }
+    input_default();
+*/
+    
+    
+    // Traitement du formulaire
+    $(function(){
+
+        
+        $(function(){
+            
+            // Traitement des champs requis
+            $champ_requis.on('keyup', function(){
+                if ($champ_requis.val() !== ""){
+                    $(this).input_success();
+                }  
+                else if ($champ_requis.val() == ""){
+            $("#ffirst-name").on('keyup', function(){
+                if ($("#first-name").val() == ""){
+                    $("first-name").css( "border-color", "red" );
+
+                }
+            });
+                /*else if ($champ_requis.val() == ""){
+                    $(this).input_vide();
+                }
+                else {
+                    $(this).input_default();
+                }
+                }*/
+            });
+    
+            // Traitement des champs alphabetiques
+            /* Traitement des champs alphabetiques
+            $champ_alpha.on('keyup', function(){
+                if ($champ_alpha.val().match(pattern_alpha)){
+                    $(this).input_success();
+@@ -96,11 +102,10 @@ $(document).ready(function() {
+                else {
+                    $(this).input_default();
+                }
+            });
+            });*/
+
+        
+       
+    });
+    
+    
 });
